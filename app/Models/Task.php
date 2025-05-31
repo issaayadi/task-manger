98be3cf7c078033a61dfd4b25dbd4c8d6dc4801d
Issa Ayadi <issaayadi2016@gmail.com>
2025-05-31 18:36:30 +0100

    Associating tasks with users

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     //
+
+    protected $fillable = ['title', 'description', 'status', 'priority', 'due_date'];
+    
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
 }
